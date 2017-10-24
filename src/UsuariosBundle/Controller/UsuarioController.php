1d9191ea5fc08e1a13c6d6941b3bbe97e0431f04
ManuelRoman <elmanuer@gmail.com>
2017-10-24 22:52:27 +0200

    Terminada la gestión de usuarios, añadido y configurado KnpPaginatorBundle

--- src/UsuariosBundle/Controller/UsuarioController.php
+++ src/UsuariosBundle/Controller/UsuarioController.php
@@ -81,4 +81,95 @@ class UsuarioController extends Controller
         
     }
     
+    /**
+     * Método sin paginador
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function listarAdmins2Action(){
+        $em = $this->getDoctrine()->getManager();
+        $usuario_repository=$em->getRepository("UsuariosBundle:Usuario");
+        $administradores = $usuario_repository->findBy(array('role' => 'ROLE_ADMIN'));
+        return $this->render("UsuariosBundle:Usuario:eliminarEditar.html.twig", array(
+            "administradores" =>$administradores
+        ));
+    }
+    
+    public function listarAdminsAction(Request $request){
+        $em = $this->getDoctrine()->getManager();
+        //Realizamos la consulta a bbdd
+        $dql   = "SELECT u FROM UsuariosBundle:Usuario u";
+        $query = $em->createQuery($dql);
+        
+        //creamos el $paginator que llama el método get de KnpPaginatorBundle
+        $paginator  = $this->get('knp_paginator');
+        //le pasamos a $paginator los parámetros y los asignamos a $pagination
+        $pagination = $paginator->paginate(
+            $query, //origen de los datos
+            $request->query->get('page', 1), //número de página por la que empieza
+            3 // límite de resultados por página
+            );
+        return $this->render('UsuariosBundle:Usuario:eliminarEditarPaginador.html.twig', array(
+            'pagination' => $pagination
+        ));
+    }
+    
+    public function eliminarAdminAction(Request $request, $id){
+        $em = $this->getDoctrine()->getManager();
+        $usuario_repository=$em->getRepository("UsuariosBundle:Usuario");
+        $usuario = $usuario_repository->find($id);
+        $em->remove($usuario);
+        $flush = $em->flush();
+        if($flush==null){
+            $status = "El administrador se ha eliminado correctamente.";
+        }else{
+            $status = "Error al eliminar el administrador.";
+        }
+        $this->session->getFlashBag()->add("status", $status);
+        return $this->render("UsuariosBundle:Usuario:inicio.html.twig");
+    }
+    
+    public function editarAdminAction(Request $request, $id){
+        $em = $this->getDoctrine()->getManager();
+        $usuario_repository=$em->getRepository("UsuariosBundle:Usuario");
+        $usuario = $usuario_repository->find($id);
+        
+        //Creamos el formularío y le damos el usuario, nos rellena los campos en la vista
+        $form = $this->createForm(UsuarioType::class, $usuario);
+        
+        //Comprobamos los campos, en este caso no hay validation
+        $form->handleRequest($request);
+        
+        //Si se ha recibido el formulario de editar el administrador
+        if($form->isSubmitted()){
+            if($form->isValid()){
+                //cambiamos los campos del administrador
+                $usuario->setNombre($form->get("nombre")->getData());
+                $usuario->setApellido($form->get("apellido")->getData());
+                $usuario->setEmail($form->get("email")->getData());
+                //Ciframos la contraseña
+                $factory = $this->get("security.encoder_factory");
+                $encoder = $factory->getEncoder($usuario);
+                $password = $encoder->encodePassword($form->get("password")->getData(), $usuario->getSalt());
+                $usuario->setPassword($password);
+                $em->persist($usuario);
+                $flush = $em->flush();
+                
+                if($flush==null){
+                    $status = "El administrador se ha editado correctamente.";
+                }else{
+                    $status = "Error al editar el administrador.";
+                }
+            }else{
+                $status = "El administrador no se ha editado, compruebe los campos.";
+            }
+            //Enviamos el mensaje de información
+            $this->session->getFlashBag()->add("status", $status);
+            //Cuando el administrador se ha editado correctamente
+            return $this->redirectToRoute("inicio");
+        }
+        
+        return $this->render("UsuariosBundle:Usuario:formularioEditar.html.twig", array(
+            "form" =>$form->createView()
+        ));
+    }
 }
